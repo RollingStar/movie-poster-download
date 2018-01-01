2e08f5d2a57202f4fc35b5f803a846a3f7982af6
RollingStar <RollingStar@users.noreply.github.com>
2018-01-01 18:51:40 -0500

    3

--- prog.py
+++ prog.py
@@ -3,12 +3,19 @@ import wget
 import os
 import datetime
 import json
+import unicodecsv
+import urllib
+import warnings
 from time import gmtime, strftime
+
+# https://developers.themoviedb.org/3/configuration/get-api-configuration
+poster_width = str(154)
 
 cwd = os.getcwd()
 
 api_file = open("api.txt")
 key = api_file.read()
+BASE_URL="https://api.themoviedb.org/3/search/movie?api_key=" + key + "&query="
 
 #https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison/29247821
 def normalize_caseless(text):
@@ -26,30 +33,68 @@ def sanitize_query(text):
     return text
 
     
-def search(url):
-    time = strftime("%Y-%m-%d_%H_%M_%S", gmtime())
-    out_file = "search_result_" + time
+def search_movie(movie_title):
+    #might or may not make sense to normalize the title before we search
+    url = BASE_URL + movie_title.replace(" ", "+")
+    out_file = "search_result_" + str(movie_title) + ".json"
+    #don't download the json if we already have results for the same search
+    if os.path.isfile(out_file):
+        return out_file
     wget.download(url, out_file)
     return out_file
 
     
-def find_title_in_json(file, title):
-    myfile = open(file)
+def find_title_in_json(json_file, movie_title):
+    myfile = open(json_file, encoding="utf-8")
     j = json.loads(myfile.read())
+    
+    def iterate_through_results(json_file, movie_title):
+        max_ind = json_file['total_results']
+        for i in range(0, max_ind):
+            print(i)
+            if caseless_equal(movie_title, json_file['results'][i]["title"]):
+                return i
+            #we couldn't find a match
+            return -1
+            
     if (j['total_results'] == 1):
         result_ind = 0
-    j = j['results'][result_ind]
-    if caseless_equal(title, j["title"]):
-        return j
-    return("error")
+    else:
+        print("multi results")
+        result_ind = iterate_through_results(j, movie_title)
+    if result_ind == -1:
+        warnings.warn("title match not found in JSON search results. possible foreign language title?")
+        #just return the first result and hope for the best
+        result_ind = 0
+    single_json = j['results'][result_ind]
+    if caseless_equal(movie_title, single_json["title"]):
+        return single_json
+    return single_json
 
+def get_json(movie_title):
+    search_result = search_movie(movie_title)
+    j = find_title_in_json(search_result, movie_title)
+    return j
 
-def get_json(title):
-    url="https://api.themoviedb.org/3/search/movie?api_key=" + key + "&query="
-    url = url + title
-    #out_file = search(url)
-    out_file="search_result"
-    j = find_title_in_json(out_file, title)
-    print(j["poster_path"])
+def download_poster(movie_title, rating=None):
+    j = get_json(movie_title)
+    poster_url = "https://image.tmdb.org/t/p/w" #+ poster_width #+ j["poster_path"]
+    print(poster_url)
+    if rating:
+        out_filename = "\\" + str(rating) + "\\" + j["title"] + ".jpg"
+    else:
+        out_filename = j["title"] + ".jpg"
+    if os.path.isfile(out_filename) == False:
+        wget.download(poster_url, out_filename)
 
-get_json("ratatouille")
+def download_posters(movies_csv):
+#movies_csv is a list of movie titles alongside their rating.    
+    with open(movies_csv, 'rb') as f_input:
+        movies = unicodecsv.reader(f_input, encoding='utf-8-sig', delimiter=',', quotechar='"')
+        for movie, rating in movies:
+            if movies.line_num <= 10:
+                print(movie + " " + rating)
+                download_poster(movie)
+            #download_poster(movie, rating)
+
+download_posters("movies.csv")
