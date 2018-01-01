78bb473dd6b592abbe391010ec3dd89923740e20
RollingStar <RollingStar@users.noreply.github.com>
2018-01-01 17:02:56 -0500

    2

--- prog.py
+++ prog.py
@@ -2,48 +2,54 @@ import unicodedata
 import wget
 import os
 import datetime
+import json
 from time import gmtime, strftime
 
 cwd = os.getcwd()
 
 api_file = open("api.txt")
 key = api_file.read()
-print(key)
 
+#https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison/29247821
+def normalize_caseless(text):
+    return unicodedata.normalize("NFKD", text.casefold())
+
+    
+def caseless_equal(left, right):
+    return normalize_caseless(left) == normalize_caseless(right)
+
+    
 def sanitize_query(text):
     #https://stackoverflow.com/questions/2365411/python-convert-unicode-to-ascii-without-errors
     text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
     text = text.replace(" ", "+")
     return text
 
+    
 def search(url):
     time = strftime("%Y-%m-%d_%H_%M_%S", gmtime())
-    out_file = cwd + "_" + time
+    out_file = "search_result_" + time
     wget.download(url, out_file)
-    response = open(out_file)
-    print(response)
-    #or result in results:
-    #    if result.title == title:
-    #        return result
+    return out_file
 
-url="https://api.themoviedb.org/3/search/movie?api_key=" + key + "&query="
-search(url)
+    
+def find_title_in_json(file, title):
+    myfile = open(file)
+    j = json.loads(myfile.read())
+    if (j['total_results'] == 1):
+        result_ind = 0
+    j = j['results'][result_ind]
+    if caseless_equal(title, j["title"]):
+        return j
+    return("error")
+
 
 def get_json(title):
     url="https://api.themoviedb.org/3/search/movie?api_key=" + key + "&query="
     url = url + title
-    json = wget(url, title)
-    return json
+    #out_file = search(url)
+    out_file="search_result"
+    j = find_title_in_json(out_file, title)
+    print(j["poster_path"])
 
-def get_poster(title, rating):
-    json = get_json(title)
-    movie_num = json.movie_num
-    url = "https://api.themoviedb.org/3/movie/" + movie_num + "?api_key=" + key
-    #json = do_wget(url)
-    #poster_url = 
-    #wget url/150/json.url -> /rating/title.jpg
-
-#title, link, rating
-#if json.title == title:
-#	wget url/150/json.url -> /rating/title.jpg
-	
+get_json("ratatouille")
